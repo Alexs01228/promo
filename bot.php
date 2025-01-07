caf536b44fb5a7e0fb5d932481dec0f205198129
Alexs01228 <rhus01228@gmail.com>
2025-01-07 17:46:28 -0600

    Add files via upload

---
+++ bot.php
@@ -0,0 +1,17 @@
+<?php
+$content = file_get_contents("php://input");
+$update = json_decode($content, true);
+
+$chat_id = $update["message"]["chat"]["id"];
+$message = $update["message"]["text"];
+
+if($message == "/start") {
+    sendMessage($chat_id, "Welcome to your new bot!");
+}
+
+function sendMessage($chat_id, $message) {
+    $apiToken = "7755443446:AAGNiOoQe0dJpKP_kB0rzkLq29WD0uBqYA8";
+    $url = "https://api.telegram.org/bot$apiToken/sendMessage?chat_id=$chat_id&text=".urlencode($message);
+    file_get_contents($url);
+}
+?>
